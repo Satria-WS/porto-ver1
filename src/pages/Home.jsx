083a5f7e3962a3b6112601734ee9d0ee2fd72bf8
Satria-WS <extencezone02@gmail.com>
2024-10-28 14:26:57 +0700

    update home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,64 @@
-import Button from "@mui/material/Button";
 import logo from "../assets/logo/logo.svg";
+import imageOrang from "../assets/images/orang.png";
+import MenuIcon from "@mui/icons-material/Menu";
+import Typography from "@mui/material/Typography";
+
+
 
 const Home = () => {
   return (
     <div>
-      <div className="">
-  
-        <div>
-          <img  src={logo} alt="logo" />
-          <span className="border">Personal</span>
+      <div className="flex justify-between border border-y-orange-900 p-3">
+        <div className="flex border border-y-orange-900 items-center">
+          <img src={logo} alt="logo" />
+          <span className="text-xl font-bold tracking-tight">Personalx</span>
         </div>
-        
+        <MenuIcon fontSize="large" />
+      </div>
+      <div className="flex flex-col justify-center items-center min-w-[300px] ">
+        <div className="orang max-w-[300px]">
+          <img src={imageOrang} alt="orang" />
+          <div className="w-[95%] bg-black h-[2px] m-auto"></div>
+        </div>
+        <div className="Biography  flex flex-col items-center  py-10  ">
+          <div className="title text-2xl tracking-tight">
+            <p>
+              Hello I’am
+              <span className="font-extrabold pl-3">Evren Shah.</span>
+            </p>
+            <p>
+              <span className="font-extrabold pr-3">Frontend</span>
+              <span>
+                <Typography
+                  sx={{
+                    fontSize: "28px",
+                    fontWeight: "bold",
+                    color: "transparent",
+                    WebkitTextStroke: "1px #000000",
+                    fontFamily: "Arial, sans-serif",
+                    display: "inline-block",
+                  }}
+                >
+                  Developer
+                </Typography>
+              </span>
+            </p>
+            <p>
+              Based In <span className="font-extrabold pl-3">India.</span>
+            </p>
+          </div>
+          <div className="content py-5">
+            <p className="px-10">
+              I'm Evren Shah Lorem Ipsum is simply dummy text of the printing
+              and typesetting industry. Lorem Ipsum has been the industry's
+              standard dummy text ever since the 1500s, when an unknown printer
+              took a galley of type and scrambled it to specimen book.
+            </p>
+          </div>
+          <div className="icon ">
+
+          </div>
+        </div>
       </div>
     </div>
   );
