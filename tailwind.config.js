23d89992a1926ed583e70b3cf225f2f96aef4115
Satria-WS <extencezone02@gmail.com>
2024-10-30 16:10:19 +0700

    custom css tailwind box-shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,19 @@ module.exports = {
   theme: {
     extend: {
       textStrokeWidth: {
-        '1.4': '1.4px'
-      }
+        '1.4': '1.4px #000000'
+      },
+      boxShadow: {
+        custom:'0px 6px 8px -6px rgba(24, 39, 75, 0.12), 0px 8px 16px -6px rgba(24, 39, 75, 0.08)',
+      },
+      
     },
   },
   plugins: [
     function ({ addUtilities }) {
       addUtilities({
         '.text-stroke-1-4': {
-          '-webkit-text-stroke-width': '1.4px'
+          '-webkit-text-stroke': '1.4px #000000',
         }
       })
     }
